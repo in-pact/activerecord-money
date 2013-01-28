4be754be3676f682c57eba2baf867854c8f1e7b1
Vlad Verestiuc <vlad.verestiuc@me.com>
2013-01-28 15:23:18 +0200

    Initial import

---
+++ lib/activerecord-money.rb
@@ -0,0 +1,78 @@
+require "activerecord-money/version"
+
+module ActiveRecord::Money
+  extend ActiveSupport::Concern
+
+
+
+  def default_money_currency(options)
+    options[:default_currency]||::Money.default_currency
+  end
+
+  def currency_field(name,options)
+    currency_field = options[:currency]||:"#{name}_currency"
+  end
+
+  def money_currency(name,options)
+    attribute_name = currency_field(name,options)
+    default_currency = default_money_currency(options)
+
+    currency = respond_to?(attribute_name) ? send(attribute_name) : nil
+    currency || default_currency
+  end
+
+  def blank_money?(value,options)
+    options[:allow_nil] and value.blank?
+  end
+
+  def money_reader(name,options={})
+    value = send("#{name}_in_cents")
+    blank_money?(value,options) ? nil : Money.new(value||0,money_currency(name,options))
+  end
+
+  def money_writer(name,value,options={})
+
+    currency_attribute_name = currency_field(name,options)
+    currency_name = money_currency(name,options)
+
+    if value.is_a? Money
+      send("#{name}_in_cents=",value.cents)
+      send("#{currency_attribute_name}=","#{value.currency.id}") if respond_to? "#{currency_attribute_name}="
+    elsif !value.blank?
+      value = value.to_money(currency_name) rescue nil
+      money_writer(name,value,options)
+    else
+      money_writer(name,0.to_money(currency_name),options) unless blank_money?(name,options)
+    end
+
+    value
+
+  end
+
+
+
+  module ClassMethods
+
+    def money(name,options={})
+      class_eval(<<-eos,__FILE__,__LINE__)
+      def #{name}
+        money_reader(:#{name},#{options.inspect})
+      end
+
+      def #{name}=(value)
+        money_writer(:#{name},value,#{options.inspect})
+      end
+
+      eos
+
+    end
+  end
+
+end
+
+
+class Money
+  def -@
+    Money.new(-cents,currency)
+  end
+end
---
+++ lib/activerecord-money/version.rb
@@ -0,0 +1,5 @@
+module Activerecord
+  module Money
+    VERSION = "0.0.1"
+  end
+end
