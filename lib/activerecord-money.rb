6c645f57a109ae3811c8b6c72b03f12a535e6636
Vlad Verestiuc <verestiuc.vlad@gmail.com>
2015-09-25 14:31:52 +0300

    Cosmetic cleanup

--- lib/activerecord-money.rb
+++ lib/activerecord-money.rb
@@ -3,58 +3,51 @@ require "activerecord-money/version"
 module ActiveRecord::Money
   extend ActiveSupport::Concern
 
-
-
   def default_money_currency(options)
-    options[:default_currency]||::Money.default_currency
+    options[:default_currency] ||= Money.default_currency
   end
 
-  def currency_field(name,options)
-    currency_field = options[:currency]||:"#{name}_currency"
+  def currency_field(name, options)
+    options[:currency] || :"#{name}_currency"
   end
 
-  def money_currency(name,options)
-    attribute_name = currency_field(name,options)
+  def money_currency(name, options)
+    attribute_name = currency_field(name, options)
     default_currency = default_money_currency(options)
 
     currency = respond_to?(attribute_name) ? send(attribute_name) : nil
     currency || default_currency
   end
 
-  def blank_money?(value,options)
+  def blank_money?(value, options)
     options[:allow_nil] and value.blank?
   end
 
-  def money_reader(name,options={})
+  def money_reader(name, options = {})
     value = send("#{name}_in_cents")
-    blank_money?(value,options) ? nil : Money.new(value||0,money_currency(name,options))
+    blank_money?(value, options) ? nil : Money.new(value || 0, money_currency(name, options))
   end
 
-  def money_writer(name,value,options={})
-
-    currency_attribute_name = currency_field(name,options)
-    currency_name = money_currency(name,options)
+  def money_writer(name, value, options = {})
+    currency_attribute_name = currency_field(name, options)
+    currency_name = money_currency(name, options)
 
     if value.is_a? Money
-      send("#{name}_in_cents=",value.cents)
-      send("#{currency_attribute_name}=","#{value.currency.id}") if respond_to? "#{currency_attribute_name}="
+      send("#{name}_in_cents=", value.cents)
+      send("#{currency_attribute_name}=", "#{value.currency.id}") if respond_to? "#{currency_attribute_name}="
     elsif !value.blank?
-      value = value.to_money(currency_name) rescue nil
-      money_writer(name,value,options)
+      value = value.to_money(currency_name)
+      money_writer(name, value, options)
     else
-      money_writer(name,0.to_money(currency_name),options) unless blank_money?(name,options)
+      money_writer(name, 0.to_money(currency_name), options) unless blank_money?(name, options)
     end
 
     value
-
   end
 
-
-
   module ClassMethods
-
-    def money(name,options={})
-      class_eval(<<-eos,__FILE__,__LINE__)
+    def money(name, options = {})
+      class_eval(<<-EOS, __FILE__, __LINE__)
       def #{name}
         money_reader(:#{name},#{options.inspect})
       end
@@ -63,16 +56,15 @@ module ActiveRecord::Money
         money_writer(:#{name},value,#{options.inspect})
       end
 
-      eos
-
+      EOS
     end
   end
-
 end
 
-
 class Money
+
   def -@
-    Money.new(-cents,currency)
+    Money.new(-cents, currency)
   end
+
 end
